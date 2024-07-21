392327380ab40448d7d8dd11c6671150bde11db6
Steven Goodwin <marquisdegeek@gmail.com>
2024-07-21 15:28:34 +0100

    Fixes and improvements to messages and queue

--- src/midi_message.js
+++ src/midi_message.js
@@ -1,3 +1,6 @@
+const midi_info = require('midi-info');
+
+
 function MIDIMessage(timeElapse, channel, messageData) {
     return {
         t: timeElapse,
@@ -7,6 +10,32 @@ function MIDIMessage(timeElapse, channel, messageData) {
 }
 
 
+function isNoteOn(data) {
+    if ((data[0] & 0xf0) === midi_info.Constants.Messages.NOTE_ON && data.length > 2 && data[2] !== 0) {
+        return true;
+    }
+
+    return false;
+}
+
+
+function isNoteOff(data) {
+    if ((data[0] & 0xf0) === midi_info.Constants.Messages.NOTE_OFF) {
+        return true;
+    }
+
+    if ((data[0] & 0xf0) === midi_info.Constants.Messages.NOTE_ON && data.length > 2 && data[2] === 0) {
+        return true;
+    }
+    return false;
+}
+
+
 module.exports = {
+    // Objects
     MIDIMessage,
+
+    // Helper methods
+    isNoteOn,
+    isNoteOff
 };
--- src/midi_message_queue.js
+++ src/midi_message_queue.js
@@ -1,5 +1,6 @@
 const MIDIMessageQueue = function() {
     const midi_info = require('midi-info');
+    const MIDIMessage = require('./midi_message');
     let queue = []; // each object has t:<time> c:<channel> d:<data>
 
 
@@ -21,7 +22,7 @@ const MIDIMessageQueue = function() {
                 // TOOD
                 // I think the correct is to match Offs to Ons, by tracking them like the keyboard input
                 // module
-                if (q.d[0] === midi_info.Constants.Messages.NOTE_OFF) {
+                if (MIDIMessage.isNoteOff(q.d)) {
                     nextQueue.push(q);
                 }
             } else {
@@ -40,22 +41,26 @@ const MIDIMessageQueue = function() {
         let offNoteIndicesToAlsoRemove = [];
 
         queue.forEach((q, idx) => {
-            let readdThisMessage = true;
+            let reAddThisMessage = true;
 
             if (q.c === channel && q.t === atTime) {
                 // Only the NoteOn msgs
-                if ((q.d[0] & 0xf0) === midi_info.Constants.Messages.NOTE_ON) {
-                    readdThisMessage = false;
+                if (MIDIMessage.isNoteOn(q.d)) {
+                    reAddThisMessage = false;
 
                     // What specifc note off are we looking for?
                     let pitch = q.d[1];
 
                     // Find the corresponding Off
-                    queue.forEach((qoff, offIdx) => {
-                        if ((qoff.d[0] & 0xf0) === midi_info.Constants.Messages.NOTE_OFF) {
+                    queue.some((qoff, offIdx) => {
+                        if (MIDIMessage.isNoteOff(qoff.d)) {
                             let offPitch = q.d[1];
                             if (pitch === offPitch) {
                                 offNoteIndicesToAlsoRemove.push(offIdx);
+                                // Stop after the 'off' has been found, as we don't
+                                // want to remove another 'off' for an 'on' note that hasn't
+                                // yet been played
+                                return true;
                             }
                         }
                     });
@@ -64,11 +69,11 @@ const MIDIMessageQueue = function() {
 
             // And any NoteOffs, that were played at t=0
             if (offNoteIndicesToAlsoRemove.indexOf(idx) !== -1) {
-                readdThisMessage = false;
+                reAddThisMessage = false;
             }
 
             //            
-            if (readdThisMessage) {
+            if (reAddThisMessage) {
                 nextQueue.push(q);
             }
         });
@@ -80,6 +85,13 @@ const MIDIMessageQueue = function() {
     function forEach(cbfn) {
         queue.forEach((q, idx) => {
             cbfn(q, idx);
+        });
+    }
+    
+    
+    function some(cbfn) {
+        return queue.some((q, idx) => {
+            return cbfn(q, idx);
         });
     }
     
@@ -99,6 +111,7 @@ const MIDIMessageQueue = function() {
         clearNotePairsAtTime,
         replaceQueue,
         forEach,
+        some,
         addMessage
     };
 }
